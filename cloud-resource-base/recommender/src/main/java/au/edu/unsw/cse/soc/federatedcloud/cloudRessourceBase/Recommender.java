5896d9cb8d29325ebc35be3806b9cb6e0a9683fa
Denis Weerasiri <ddweerasiri@gmail.com>
2014-02-10 17:07:57 +1100

    Modified poms to avoid duplicate logger dependencies and added the orchestrator engine

---
+++ cloud-resource-base/recommender/src/main/java/au/edu/unsw/cse/soc/federatedcloud/cloudRessourceBase/Recommender.java
@@ -0,0 +1,71 @@
+package au.edu.unsw.cse.soc.federatedcloud.cloudRessourceBase;
+/*
+ * Copyright (c) 2014, Denis Weerasiri All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * User: denis
+ * The recommender API
+ */
+@Path("/recommender")
+public class Recommender {
+    private static final Logger log = LoggerFactory.getLogger(Recommender.class);
+
+    @Path("/getAll")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllRecommendations() {
+        JsonObject json = new JsonObject();
+        JsonArray descriptionsJsonArray = new JsonArray();
+
+        JsonObject desciption1 = new JsonObject();
+        desciption1.addProperty("Id", "1");
+        desciption1.addProperty("Name", "SE-Bucket");
+        desciption1.addProperty("TargetEnv", "IaaS");
+        desciption1.addProperty("Deployer", "au.edu.unsw.cse.soc.federatedcloud.deployers.aws.AWSS3Deployer");
+        descriptionsJsonArray.add(desciption1);
+
+        JsonObject desciption2 = new JsonObject();
+        desciption2.addProperty("Id", "2");
+        desciption2.addProperty("Name", "Rackspace-VM");
+        desciption2.addProperty("TargetEnv", "IaaS");
+        desciption2.addProperty("Deployer", "au.edu.unsw.cse.soc.federatedcloud.deployers.rackspace.RackspaceDeployer");
+        descriptionsJsonArray.add(desciption2);
+
+        JsonObject desciption3 = new JsonObject();
+        desciption3.addProperty("Id", "3");
+        desciption3.addProperty("Name", "GoogleDrive");
+        desciption3.addProperty("TargetEnv", "SaaS");
+        desciption3.addProperty("Deployer", "au.edu.unsw.cse.soc.federatedcloud.deployers.GoogleDriveDeployer");
+        descriptionsJsonArray.add(desciption3);
+
+        json.add("CloudResourceDescriptions", descriptionsJsonArray);
+
+        return Response.status(Response.Status.OK).entity(json.toString()).build();
+
+    }
+}
---
+++ orchestration-engine/src/main/java/au/edu/unsw/cse/soc/federatedcloud/orchestrator/Engine.java
@@ -0,0 +1,48 @@
+package au.edu.unsw.cse.soc.federatedcloud.orchestrator;
+/*
+ * Copyright (c) 2014, Denis Weerasiri All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+/**
+ * User: denis
+ * Represents the Orchestrator Engine
+ */
+public class Engine {
+    private static final Logger log = LoggerFactory.getLogger(Engine.class);
+    ExecutionContext context = new ExecutionContext();
+
+    public static void main(String[] args) {
+
+    }
+
+    public void executeTransition(Event event) {
+        List<Transition> transitions = null;
+        for (Transition t : transitions) {
+            if (context.getActiveState().getID() == t.getSourceState().getID()) {
+                if (event.getID() == t.getECA().getEvent().getID()) {
+                    if (t.getECA().getCondition() == true) {
+                        triggerTransition(t);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
